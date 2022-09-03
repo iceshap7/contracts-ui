47060fa87fdbd8a8605aa2c23c9e37a2934c465a
Tien Trinh <tientd@caerux.com>
2022-09-04 04:09:13 +0700

    deposit node updated

--- src/ui/components/flow/Nodes/DepositNode.tsx
+++ src/ui/components/flow/Nodes/DepositNode.tsx
@@ -1,46 +1,32 @@
 import { useContext, useEffect, useState } from 'react';
 import { Handle, Position } from 'react-flow-renderer';
+import DateTimePicker from 'react-datetime-picker';
+
 import { AccountSelectSimple } from 'ui/components/account/SelectSimple';
 import { EditorContext } from 'ui/contexts/EditorContext';
 import { useAccountId } from 'ui/hooks';
 
 const DepositNote = () => {
-  const { value: fromAccountId, onChange: setFromAccountId } = useAccountId();
   const { value: toAccountId, onChange: setToAccountId } = useAccountId();
-  const [amount, setAmount] = useState('0');
+  const [withdrawAt, setWithdrawAt] = useState(new Date());
   const { code, setCode } = useContext(EditorContext);
 
   useEffect(() => {
     setCode({
       type: 'deposit',
-      amount,
-      fromAccountId,
+      withdrawAt: new Date(withdrawAt).getTime(),
       toAccountId,
     });
   }, []);
 
   return (
     <div className="c-node">
-      <p className="font-bold border-b-2">Deposit</p>
+      <p className="font-bold border-b-2 mb-2">Deposit</p>
 
       <div className="nodrag cursor-auto">
-        <div className="grid grid-cols-2 gap-2 mb-2">
-          <div>
-            From:{' '}
-            <AccountSelectSimple
-              id="fromAccountId"
-              className="mb-2"
-              value={fromAccountId}
-              onChange={accountId => {
-                setFromAccountId(accountId);
-                setCode(prev => {
-                  return { ...prev, fromAccountId: accountId };
-                });
-              }}
-            />
-          </div>
-          <div>
-            To:{' '}
+        <div className="grid grid-cols-10 gap-3 items-center mb-2">
+          <div className="col-span-1"><label>To:</label></div>
+          <div className="col-span-9">
             <AccountSelectSimple
               id="toAccountId"
               className="mb-2"
@@ -56,16 +42,15 @@ const DepositNote = () => {
         </div>
 
         <div className="mb-2">
-          <label className="mr-[5px]">Amount:</label>
-          <input
-            className="px-[5px] py-[2px] text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
-            value={amount}
-            onChange={({ target }) => {
-              setAmount(target.value);
+          <label className="mr-[5px]">Withdraw at:</label>
+          <DateTimePicker
+            onChange={value => {
+              setWithdrawAt(value);
               setCode(prev => {
-                return { ...prev, amount: target.value };
+                return { ...prev, withdrawAt: new Date(value).getTime() };
               });
             }}
+            value={withdrawAt}
           />
         </div>
       </div>
